6ddd22a9bd4602768b9f6bf85f7714f0a16f43fe
Trim21 <trim21.me@gmail.com>
2021-12-08 18:46:05 +0800

    fix data

--- sql/data/data_chii_person_cs_index.sql
+++ sql/data/data_chii_person_cs_index.sql
@@ -25,9 +25,9 @@ SET time_zone = "+00:00";
 --
 
 INSERT INTO `chii_person_cs_index` (`prsn_type`, `prsn_id`, `prsn_position`, `subject_id`, `subject_type_id`, `summary`, `prsn_appear_eps`) VALUES
-('prsn', 1, 1001, 4, 2, '', ''),
-('prsn', 1, 1002, 5, 2, '', ''),
-('prsn', 1, 1003, 6, 2, '', ''),
+('prsn', 1, 1001, 4, 4, '', ''),
+('prsn', 1, 1002, 5, 4, '', ''),
+('prsn', 1, 1003, 6, 4, '', ''),
 ('prsn', 1, 34, 1264, 2, '', ''),
 ('prsn', 1, 33, 220187, 2, '', ''),
 ('prsn', 1, 3001, 12105, 3, '', ''),
