210fd12e008accc3f1e8e56927caeca3d7f69cb8
Trim21 <trim21.me@gmail.com>
2021-12-24 02:14:59 +0800

    add a test user group

---
+++ sql/data/chii_usergroup.sql
@@ -0,0 +1,5 @@
+INSERT INTO `chii_usergroup` (`usr_grp_id`, `usr_grp_name`, `usr_grp_perm`, `usr_grp_dateline`) VALUES
+(10, '普通用户', 'O:8:"stdClass":14:{s:9:"user_list";s:1:"1";s:17:"manage_user_group";s:1:"1";s:11:"manage_user";s:1:"1";s:19:"doujin_
+subject_lock";s:1:"1";s:12:"subject_edit";s:1:"1";s:12:"subject_lock";s:1:"1";s:15:"subject_refresh";s:1:"1";s:15:"s
+ubject_related";s:1:"1";s:10:"mono_merge";s:1:"1";s:10:"mono_erase";s:1:"1";s:7:"ep_edit";s:1:"1";s:7:"ep_move";s:1:
+"1";s:6:"report";s:1:"1";s:9:"app_erase";s:1:"1";}', 1304011366)
