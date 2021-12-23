1defd4ca816bee54c1d45709fa2e3fd5e2b1ca22
Trim21 <Trim21@users.noreply.github.com>
2021-12-23 16:11:10 +0000

    deploy refs/heads/main 85e08efc9c63ec5a332f6317dfbcb326b63ccb4b

--- dist.sql
+++ dist.sql
@@ -294,6 +294,371 @@ ALTER TABLE `chii_person_collects`
 -- http://www.phpmyadmin.net
 --
 -- Host: 192.168.201.71
+-- Generation Time: Dec 23, 2021 at 09:10 AM
+-- Server version: 5.7.33-0ubuntu0.16.04.1-log
+-- PHP Version: 5.5.9-1ubuntu4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bangumi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chii_usergroup`
+--
+
+CREATE TABLE IF NOT EXISTS `chii_usergroup` (
+  `usr_grp_id` mediumint(8) unsigned NOT NULL,
+  `usr_grp_name` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `usr_grp_perm` mediumtext CHARACTER SET utf8 NOT NULL,
+  `usr_grp_dateline` int(10) unsigned NOT NULL
+) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Indexes for table `chii_usergroup`
+--
+ALTER TABLE `chii_usergroup`
+  ADD PRIMARY KEY (`usr_grp_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `chii_usergroup`
+--
+ALTER TABLE `chii_usergroup`
+  MODIFY `usr_grp_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=12;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- phpMyAdmin SQL Dump
+-- version 4.4.15.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 192.168.201.71
+-- Generation Time: Dec 23, 2021 at 08:54 AM
+-- Server version: 5.7.33-0ubuntu0.16.04.1-log
+-- PHP Version: 5.5.9-1ubuntu4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bangumi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chii_subject_topics`
+--
+
+CREATE TABLE IF NOT EXISTS `chii_subject_topics` (
+  `sbj_tpc_id` mediumint(8) unsigned NOT NULL,
+  `sbj_tpc_subject_id` mediumint(8) unsigned NOT NULL,
+  `sbj_tpc_uid` mediumint(8) unsigned NOT NULL,
+  `sbj_tpc_title` varchar(80) CHARACTER SET utf8mb4 NOT NULL,
+  `sbj_tpc_dateline` int(10) unsigned NOT NULL DEFAULT '0',
+  `sbj_tpc_lastpost` int(10) unsigned NOT NULL DEFAULT '0',
+  `sbj_tpc_replies` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `sbj_tpc_state` tinyint(1) unsigned NOT NULL,
+  `sbj_tpc_display` tinyint(1) unsigned NOT NULL DEFAULT '1'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `chii_subject_topics`
+--
+ALTER TABLE `chii_subject_topics`
+  ADD PRIMARY KEY (`sbj_tpc_id`),
+  ADD KEY `tpc_subject_id` (`sbj_tpc_subject_id`),
+  ADD KEY `tpc_display` (`sbj_tpc_display`),
+  ADD KEY `sbj_tpc_uid` (`sbj_tpc_uid`),
+  ADD KEY `sbj_tpc_lastpost` (`sbj_tpc_lastpost`,`sbj_tpc_subject_id`,`sbj_tpc_display`) USING BTREE;
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `chii_subject_topics`
+--
+ALTER TABLE `chii_subject_topics`
+  MODIFY `sbj_tpc_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- phpMyAdmin SQL Dump
+-- version 4.4.15.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 192.168.201.71
+-- Generation Time: Dec 23, 2021 at 08:53 AM
+-- Server version: 5.7.33-0ubuntu0.16.04.1-log
+-- PHP Version: 5.5.9-1ubuntu4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bangumi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chii_group_topics`
+--
+
+CREATE TABLE IF NOT EXISTS `chii_group_topics` (
+  `grp_tpc_id` mediumint(8) unsigned NOT NULL,
+  `grp_tpc_gid` mediumint(8) unsigned NOT NULL,
+  `grp_tpc_uid` mediumint(8) unsigned NOT NULL,
+  `grp_tpc_title` varchar(80) NOT NULL,
+  `grp_tpc_dateline` int(10) unsigned NOT NULL DEFAULT '0',
+  `grp_tpc_lastpost` int(10) unsigned NOT NULL DEFAULT '0',
+  `grp_tpc_replies` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `grp_tpc_state` tinyint(1) unsigned NOT NULL,
+  `grp_tpc_display` tinyint(1) unsigned NOT NULL DEFAULT '1'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `chii_group_topics`
+--
+ALTER TABLE `chii_group_topics`
+  ADD PRIMARY KEY (`grp_tpc_id`),
+  ADD KEY `grp_tpc_gid` (`grp_tpc_gid`),
+  ADD KEY `grp_tpc_display` (`grp_tpc_display`),
+  ADD KEY `grp_tpc_uid` (`grp_tpc_uid`),
+  ADD KEY `grp_tpc_lastpost` (`grp_tpc_lastpost`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `chii_group_topics`
+--
+ALTER TABLE `chii_group_topics`
+  MODIFY `grp_tpc_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- phpMyAdmin SQL Dump
+-- version 4.4.15.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 192.168.201.71
+-- Generation Time: Dec 23, 2021 at 08:55 AM
+-- Server version: 5.7.33-0ubuntu0.16.04.1-log
+-- PHP Version: 5.5.9-1ubuntu4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bangumi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chii_crt_comments`
+--
+
+CREATE TABLE IF NOT EXISTS `chii_crt_comments` (
+  `crt_pst_id` mediumint(8) unsigned NOT NULL,
+  `crt_pst_mid` mediumint(8) unsigned NOT NULL COMMENT '关联人物ID',
+  `crt_pst_uid` mediumint(8) unsigned NOT NULL,
+  `crt_pst_related` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `crt_pst_dateline` int(10) unsigned NOT NULL,
+  `crt_pst_content` mediumtext NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chii_ep_comments`
+--
+
+CREATE TABLE IF NOT EXISTS `chii_ep_comments` (
+  `ep_pst_id` mediumint(8) unsigned NOT NULL,
+  `ep_pst_mid` mediumint(8) unsigned NOT NULL,
+  `ep_pst_uid` mediumint(8) unsigned NOT NULL,
+  `ep_pst_related` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `ep_pst_dateline` int(10) unsigned NOT NULL,
+  `ep_pst_content` mediumtext NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chii_group_posts`
+--
+
+CREATE TABLE IF NOT EXISTS `chii_group_posts` (
+  `grp_pst_id` mediumint(8) unsigned NOT NULL,
+  `grp_pst_mid` mediumint(8) unsigned NOT NULL,
+  `grp_pst_uid` mediumint(8) unsigned NOT NULL,
+  `grp_pst_related` mediumint(8) unsigned NOT NULL DEFAULT '0' COMMENT '关联回复ID',
+  `grp_pst_content` mediumtext NOT NULL,
+  `grp_pst_state` tinyint(1) unsigned NOT NULL,
+  `grp_pst_dateline` int(10) unsigned NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chii_prsn_comments`
+--
+
+CREATE TABLE IF NOT EXISTS `chii_prsn_comments` (
+  `prsn_pst_id` mediumint(8) unsigned NOT NULL,
+  `prsn_pst_mid` mediumint(8) unsigned NOT NULL COMMENT '关联人物ID',
+  `prsn_pst_uid` mediumint(8) unsigned NOT NULL,
+  `prsn_pst_related` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `prsn_pst_dateline` int(10) unsigned NOT NULL,
+  `prsn_pst_content` mediumtext NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chii_subject_posts`
+--
+
+CREATE TABLE IF NOT EXISTS `chii_subject_posts` (
+  `sbj_pst_id` mediumint(8) unsigned NOT NULL,
+  `sbj_pst_mid` mediumint(8) unsigned NOT NULL,
+  `sbj_pst_uid` mediumint(8) unsigned NOT NULL,
+  `sbj_pst_related` mediumint(8) unsigned NOT NULL DEFAULT '0',
+  `sbj_pst_content` mediumtext CHARACTER SET utf8mb4 NOT NULL,
+  `sbj_pst_state` tinyint(1) unsigned NOT NULL,
+  `sbj_pst_dateline` int(10) unsigned NOT NULL DEFAULT '0'
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `chii_crt_comments`
+--
+ALTER TABLE `chii_crt_comments`
+  ADD PRIMARY KEY (`crt_pst_id`),
+  ADD KEY `cmt_crt_id` (`crt_pst_mid`),
+  ADD KEY `crt_pst_related` (`crt_pst_related`),
+  ADD KEY `crt_pst_uid` (`crt_pst_uid`);
+
+--
+-- Indexes for table `chii_ep_comments`
+--
+ALTER TABLE `chii_ep_comments`
+  ADD PRIMARY KEY (`ep_pst_id`),
+  ADD KEY `ep_cmt_crt_id` (`ep_pst_mid`),
+  ADD KEY `ep_pst_related` (`ep_pst_related`),
+  ADD KEY `ep_pst_uid` (`ep_pst_uid`);
+
+--
+-- Indexes for table `chii_group_posts`
+--
+ALTER TABLE `chii_group_posts`
+  ADD PRIMARY KEY (`grp_pst_id`),
+  ADD KEY `pss_topic_id` (`grp_pst_mid`),
+  ADD KEY `grp_pst_related` (`grp_pst_related`),
+  ADD KEY `grp_pst_uid` (`grp_pst_uid`);
+
+--
+-- Indexes for table `chii_prsn_comments`
+--
+ALTER TABLE `chii_prsn_comments`
+  ADD PRIMARY KEY (`prsn_pst_id`),
+  ADD KEY `cmt_prsn_id` (`prsn_pst_mid`),
+  ADD KEY `prsn_pst_related` (`prsn_pst_related`),
+  ADD KEY `prsn_pst_uid` (`prsn_pst_uid`);
+
+--
+-- Indexes for table `chii_subject_posts`
+--
+ALTER TABLE `chii_subject_posts`
+  ADD PRIMARY KEY (`sbj_pst_id`),
+  ADD KEY `pss_topic_id` (`sbj_pst_mid`),
+  ADD KEY `sbj_pst_related` (`sbj_pst_related`),
+  ADD KEY `sbj_pst_uid` (`sbj_pst_uid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `chii_crt_comments`
+--
+ALTER TABLE `chii_crt_comments`
+  MODIFY `crt_pst_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `chii_ep_comments`
+--
+ALTER TABLE `chii_ep_comments`
+  MODIFY `ep_pst_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `chii_group_posts`
+--
+ALTER TABLE `chii_group_posts`
+  MODIFY `grp_pst_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `chii_prsn_comments`
+--
+ALTER TABLE `chii_prsn_comments`
+  MODIFY `prsn_pst_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `chii_subject_posts`
+--
+ALTER TABLE `chii_subject_posts`
+  MODIFY `sbj_pst_id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- phpMyAdmin SQL Dump
+-- version 4.4.15.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 192.168.201.71
 -- Generation Time: Dec 06, 2021 at 02:29 PM
 -- Server version: 5.7.33-0ubuntu0.16.04.1-log
 -- PHP Version: 5.5.9-1ubuntu4.29
