081b1e170d5c4a2c89460b515af041afdde16ec0
JarvieK <3547456+Jarvie8176@users.noreply.github.com>
2022-01-01 23:04:47 -0500

    stub for testing subject topics endpoint

---
+++ sql/data/data_subjects_topics_authz_check.sql
@@ -0,0 +1,73 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+replace into chii_subjects (`subject_id`, `subject_type_id`, `subject_name`, `subject_name_cn`, `subject_uid`,
+                            `subject_creator`, `subject_dateline`, `subject_image`, `subject_platform`, `field_infobox`,
+                            `field_summary`, `field_5`, `field_volumes`, `field_eps`, `subject_wish`, `subject_collect`,
+                            `subject_doing`, `subject_on_hold`, `subject_dropped`, `subject_series`,
+                            `subject_series_entry`, `subject_idx_cn`, `subject_airtime`, `subject_nsfw`, `subject_ban`)
+values (277554, 2, '無職転生 ～異世界行ったら本気だす～', '无职转生 ～在异世界认真地活下去～', '', 207398, 1552625104, '8b/00/277554_z999u.jpg', 1,
+        '{{Infobox animanga/TVAnime\r\n|中文名= 无职转生 ～在异世界认真地活下去～\r\n|别名={\r\n[无职转生 ～到了异世界就拿出真本事～]\r\n[英语|Jobless Reincarnation ~I Will Seriously Try If I Go to Another World~]\r\n[罗马字|Mushoku Tensei ~Isekai Ittara Honki Dasu~]\r\n}\r\n|话数= 11\r\n|放送开始= 2021年1月10日\r\n|放送星期= 星期日\r\n|官方网站= https://mushokutensei.jp\r\n|播放电视台= TOKYO MX\r\n|其他电视台= サンテレビ / KBS京都 / BS11\r\n|播放结束= \r\n|其他= \r\n|Copyright= ©理不尽な孫の手/MFブックス/「無職転生」製作委員会\r\n|原作= 理不尽な孫の手\r\n|导演= 岡本学\r\n|人物设定= 杉山和隆；副人物设定：齊藤佳子\r\n|作画监督= 山崎絵美\r\n|製作= 「無職転生」製作委員会（博報堂DYミュージック&amp;ピクチャーズ、東宝、KADOKAWA、フロンティアワークス、日本BS放送、グリー、EGG FIRM）\r\n|企画= 山崎史紀、大田圭二、青柳昌行、辻政英、太田勝士、宿輪浩介、大澤信博\r\n|总制片= 山崎史紀、大澤信博\r\n|制片人= 山中隆弘、小野田壮吉、 森井巧、今井遼介、大和田智之、大須賀翔、畠山拓郎\r\n|imdb_id= tt13293602\r\n}}',
+        '“我要在这个异世界拿出真本事！”\r\n34岁童贞且无职的家里蹲男子，在父母的葬礼当天被赶出家门后，在路上被一辆卡车所撞死。意识清醒后，他发现自己居然作为一个刚出生的婴儿转生到了剑与魔法的异世界！像废物一样活过了前世的男子，发誓要作为少年·鲁迪乌斯在异世界以认真的态度好好活下去！\r\n等待着鲁迪乌斯的是，与娇小魔术师、有着精灵长耳又很男孩子气的美少女、凶暴的傲娇大小姐以及其他性格各异的人们之间的邂逅，当然也还有残酷的冒险与战斗。\r\n新的人生就要开始了！“人生重来型”幻想冒险谭，在此开幕！',
+        '', 0, 11, 781, 6943, 1569, 269, 355, 0, 0, 'w', 0, 0, 0),
+       (226750, 2, 'ソードアート・オンライン オルタナティブ ガンゲイル・オンライン', '刀剑神域外传 Gun Gale Online', '', 207398, 1506823251,
+        '29/40/226750_3fW9q.jpg', 1,
+        '{{Infobox animanga/TVAnime\r\n|中文名= 刀剑神域外传 Gun Gale Online\r\n|别名={\r\n[Sword Art Online Alternative: Gun Gale Online]\r\n[刀剑神域外传GGO]\r\n}\r\n|话数= 12\r\n|放送开始= 2018年4月7日\r\n|放送星期= 星期六\r\n|官方网站= http://gungale-online.net/\r\n|播放电视台= TOKYO MX\r\n|其他电视台= BS11 / とちぎテレビ / 群馬テレビ / MBS / テレビ愛知 / 中華電信MOD / bbTV\r\n|播放结束= 2018年6月30日\r\n|其他= \r\n|Copyright= © 2017 時雨沢恵一／KADOKAWA アスキー・メディアワークス／GGO Project\r\n|监修= 川原礫\r\n|製作= GGO Project（Aniplex、KADOKAWA アスキー・メディアワークス、バンダイナムコエンターテインメント、ジェンコ、ストレートエッジ、EGG FIRM）\r\n|原作= 時雨沢恵一\r\n|导演= 迫井政行\r\n|人物设定= 小堺能夫\r\n}}',
+        '在枪与钢铁的世界“Gun Gale Online”享受着单人游戏的女性玩家·莲。喜欢可爱事物的她，全身清一色的粉红色装备，不断累积游戏经验，并逐渐增强实力。之后，由于某件事而对于PK（玩家狩猎）觉醒了兴趣的莲沉迷于PK中，并终于到达被人称作“粉红恶魔”的地步。\r\n这样的莲，与神秘的美女玩家·Pitohui相遇，并和她意气相投。莲按照她所说的，参加了小队作战活动“特攻强袭”。 ',
+        '', 0, 12, 511, 3466, 355, 292, 267, 0, 0, 'd', 0, 0, 0);
+
+replace into chii_subject_topics (`sbj_tpc_id`, `sbj_tpc_subject_id`, `sbj_tpc_uid`, `sbj_tpc_title`,
+                                  `sbj_tpc_dateline`,
+                                  `sbj_tpc_lastpost`, `sbj_tpc_replies`, `sbj_tpc_state`, `sbj_tpc_display`)
+values (2477, 1347, 88759, '黑车出售14789747997无需订金黑车出售', 1343161113, 1343161113, 0, 0, 2);
+
+
+replace into chii_subject_posts (`sbj_pst_id`, `sbj_pst_mid`, `sbj_pst_uid`, `sbj_pst_related`, `sbj_pst_content`,
+                                 `sbj_pst_state`, `sbj_pst_dateline`)
+values (73151, 6998, 70004, 0, '127话，好酷\n夕映  all knowing\n 和香 all perceiving\n\n明日菜和葱头的孩子么..', 6, 1466022514),
+       (73387, 6998, 70004, 0, '128话，明日菜出现！', 6, 1466639296),
+       (80114, 10416, 299900, 0, '再发个讨论串上一下首页。（我跟', 6, 1476513560),
+       (82459, 6998, 70004, 0, '131话，依文也好，明日菜也好，都很享受那几年的胡闹生活(bgm108)', 6, 1482192413),
+       (82505, 6998, 70004, 0, '130话，依文喜欢的三个人(bgm39)', 6, 1482251621),
+       (85999, 10913, 8142, 0, '一堆bug不是正说明是gust自己开发的(bgm38)', 6, 1492257183),
+       (86569, 10669, 240792, 0, '放心，根本不会有朋友约你去水下乐园玩，因为根本就没有……', 6, 1493616165),
+       (87724, 6998, 70004, 0,
+        '136话，雾绘实在是太可爱了；大晚上的，在旅店给刀太上生理卫生课(bgm38) ，打开付费频道看羞羞答答的节目。\r\n要问为什么.......雾绘心里想: 我都准备好了，可是，可是，可是，这家伙，对xx完-全-没-有-概-念！这还怎么继续开展下去！怒！',
+        6, 1495290759),
+       (88743, 6998, 70004, 0,
+        '137话，谢谢赤松大魔王！本来我就不承认魔法老师的最后三话，果然大魔王本人也不承认，所以才有了UQ Holder.\n\n''人类就是踩踏着过去而前进的动物''，如果遇到什么事情都靠读档解决，把过去的一切清零，未免，不仅仅是作弊，而且是无聊',
+        6, 1497209906),
+       (93240, 11134, 304246, 92975, '对她们来说重要的不是谁赢了，怎么活命才是最要紧的。真好呐，总比被主义冲昏了头誓死效忠去要好。', 6, 1503743410);
+
+replace into chii_subject_posts (`sbj_pst_id`, `sbj_pst_mid`, `sbj_pst_uid`, `sbj_pst_related`, `sbj_pst_content`,
+                                 `sbj_pst_state`, `sbj_pst_dateline`)
+values (132630, 15849, 259867, 0, '我不要你觉得，我要我觉得，我觉得你就是个傻哔', 7, 1567317329),
+       (132631, 15849, 259867, 0, '你觉得你馬呢？', 7, 1567317473),
+       (139361, 15849, 259867, 132630, '(quote),(b),奏のマーボードーフ ...(/b), 说: 我不要你觉得，我要我觉得，我觉得你就是个傻哔(/quote),\n我觉得你馬死了', 7,
+        1580187182),
+       (139362, 15849, 259867, 132631, '(quote),(b),奏のマーボードーフ ...(/b), 说: 你觉得你馬呢？(/quote),\n我觉得 你 馬 死 了', 7,
+        1580187247),
+       (159724, 18783, 575518, 159708, '别的东西里面有屎，不证明这部作品里的屎不是屎，快把无职拿给你领导看看，让他感受一下你的三观。', 7, 1612656554),
+       (159728, 18783, 238073, 159708,
+        '(quote),(b),人上人(/b), 说: 别的东西里面有屎，不证明这部作品里的屎不是屎，快把无职拿给你领导看看，让他感受一下你的三观。(/quote),\n幕刃上大号操你妈', 7, 1612656896),
+       (159730, 18783, 238073, 159708,
+        '(quote),(b),人上人(/b), 说: 别的东西里面有屎，不证明这部作品里的屎不是屎，快把无职拿给你领导看看，让他感受一下你的三观。(/quote),\n还有老子领导我自己，跟你这个舔鞋的可不一样', 7,
+        1612657067),
+       (159731, 18783, 210454, 159708,
+        '(quote),(b),人上人(/b), 说: 别的东西里面有屎，不证明这部作品里的屎不是屎，快把无职拿给你领导看看，让他感受一下你的三观。(/quote),\n瞎搅和', 7, 1612657115),
+       (159732, 18783, 575518, 159708,
+        '(quote),(b),Lantrancy(/b), 说: 幕刃上大号操你妈(/quote),\n哈哈哈哈看你看个德行，三观很正，你的领导一定对你的三观很感兴趣。', 7, 1612657120),
+       (159733, 18783, 575518, 159708,
+        '(quote),(b),Lantrancy(/b), 说: 还有老子领导我自己，跟你这个舔鞋的可不一样(/quote),\n领导就是不一样啊，天天和别人对线一定很闲吧？是不是失业了在家当无职领导？', 7,
+        1612657183);
+
+
+/*!40101 SET CHARACTER_SET_CLIENT = @OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS = @OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION = @OLD_COLLATION_CONNECTION */;
