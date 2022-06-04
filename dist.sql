5447719262e360287d273e231df523113f4cab55
Trim21 <Trim21@users.noreply.github.com>
2022-06-04 06:06:38 +0000

    deploy refs/heads/master afde2db64828bda9b0b6baabe80bf5a4ce2d49f6

--- dist.sql
+++ dist.sql
@@ -1503,6 +1503,22 @@ ALTER TABLE `chii_apps`
 --
 ALTER TABLE `chii_apps`
     MODIFY `app_id` mediumint(8) NOT NULL AUTO_INCREMENT;
+CREATE TABLE IF NOT EXISTS `chii_oauth_clients`
+(
+    `app_id`        mediumint(8)                        NOT NULL,
+    `client_id`     varchar(80) COLLATE utf8_unicode_ci NOT NULL,
+    `client_secret` varchar(80) COLLATE utf8_unicode_ci   DEFAULT NULL,
+    `redirect_uri`  varchar(2000) COLLATE utf8_unicode_ci DEFAULT NULL,
+    `grant_types`   varchar(80) COLLATE utf8_unicode_ci   DEFAULT NULL,
+    `scope`         varchar(4000) COLLATE utf8_unicode_ci DEFAULT NULL,
+    `user_id`       varchar(80) COLLATE utf8_unicode_ci   DEFAULT NULL
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8
+  COLLATE = utf8_unicode_ci;
+
+ALTER TABLE `chii_oauth_clients`
+    ADD PRIMARY KEY (`app_id`),
+    ADD KEY `client_id` (`client_id`);
 CREATE TABLE IF NOT EXISTS `chii_os_web_sessions`
 (
     `key`        char(64) PRIMARY KEY NOT NULL comment 'session key', #
