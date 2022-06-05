f8203a43e9ee4687d32c10d03769372feb23063f
FDKevin <fdkevinzero@gmail.com>
2022-06-06 02:38:45 +0800

    echo test

--- wait_mysql_ready.sh
+++ wait_mysql_ready.sh
@@ -3,6 +3,7 @@
 for _ in {1..60}; do
   # your-unix-command-here
   if docker exec bangumi_mysql mysql --user=user --password=password -e "SELECT 1" >/dev/null 2>&1; then
+    docker exec bangumi_mysql mysql --user=user --password=password -e "SELECT 1"
     exit 0
   fi
   sleep 0.5
