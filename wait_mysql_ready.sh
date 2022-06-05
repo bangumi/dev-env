e5d5850e1004463fe38e42cfabe43f1564203abd
FDKevin <fdkevinzero@gmail.com>
2022-06-06 02:22:52 +0800

    fix(ci test): docker mysql init check #17

--- wait_mysql_ready.sh
+++ wait_mysql_ready.sh
@@ -2,7 +2,7 @@
 
 for _ in {1..60}; do
   # your-unix-command-here
-  if mysqladmin ping -h "127.0.0.1" --silent; then
+  if docker exec bangumi-dev-env_mysql_1 mysql --user=user --password=password -e "SELECT 1" >/dev/null 2>&1; then
     exit 0
   fi
   sleep 0.5
