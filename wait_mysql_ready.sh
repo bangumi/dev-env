2ec64015f52c5a3579f58eb19868cec2d800dc3e
FDKevin <fdkevinzero@gmail.com>
2022-06-06 02:35:38 +0800

    custom container name

--- wait_mysql_ready.sh
+++ wait_mysql_ready.sh
@@ -2,13 +2,11 @@
 
 for _ in {1..60}; do
   # your-unix-command-here
-  if docker exec bangumi-dev-env_mysql_1 mysql --user=user --password=password -e "SELECT 1" >/dev/null 2>&1; then
+  if docker exec bangumi_mysql mysql --user=user --password=password -e "SELECT 1" >/dev/null 2>&1; then
     exit 0
   fi
-  sleep 1
+  sleep 0.5
 done
-
-docker exec bangumi-dev-env_mysql_1 mysql --user=user --password=password -e "SELECT 1"
 
 echo "timeout"
 
