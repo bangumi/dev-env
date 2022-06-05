b2ade2d02978aba29fec2f2283b5aa654aef264a
FDKevin <fdkevinzero@gmail.com>
2022-06-06 02:27:17 +0800

    longer timeout

--- wait_mysql_ready.sh
+++ wait_mysql_ready.sh
@@ -5,7 +5,7 @@ for _ in {1..60}; do
   if docker exec bangumi-dev-env_mysql_1 mysql --user=user --password=password -e "SELECT 1" >/dev/null 2>&1; then
     exit 0
   fi
-  sleep 0.5
+  sleep 1
 done
 
 echo "timeout"
