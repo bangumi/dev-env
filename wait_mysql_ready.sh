97a8909771c666dcdad36084626ee7c1be6b217e
FDKevin <fdkevinzero@gmail.com>
2022-06-06 18:25:29 +0800

    script dir

--- wait_mysql_ready.sh
+++ wait_mysql_ready.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd $(dirname "$0")/..
+cd $(dirname "$0")
 
 timeout 30s grep -q 'MySQL init process done. Ready for start up.' <(docker-compose logs -f)
 timeout 30s grep -q 'ready for connections' <(docker-compose logs -f)
