382fbfa2841660192f545feb06c8311864eb8fe0
FDKevin <fdkevinzero@gmail.com>
2022-06-06 10:36:02 +0800

    move to wait_mysql_ready.sh

--- wait_mysql_ready.sh
+++ wait_mysql_ready.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
-for _ in {1..60}; do
-  # your-unix-command-here
-  if docker exec bangumi_mysql mysql --user=user --password=password -e "SELECT 1" >/dev/null 2>&1; then
-    docker exec bangumi_mysql mysql --user=user --password=password -e "SELECT 1"
-    exit 0
-  fi
-  sleep 0.5
-done
-
-echo "timeout"
-
-exit 1
+timeout 30s grep -q 'MySQL init process done. Ready for start up.' <(docker-compose logs -f)
+timeout 30s grep -q 'ready for connections' <(docker-compose logs -f)
