c32e0d1579cb19d9df7e42d1965b354fdeb7a998
FDKevin <fdkevinzero@gmail.com>
2022-06-06 16:13:17 +0800

    feat: ci test with custom docker compose

--- wait_mysql_ready.sh
+++ wait_mysql_ready.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-timeout 30s grep -q 'MySQL init process done. Ready for start up.' <(docker-compose logs -f)
-timeout 30s grep -q 'ready for connections' <(docker-compose logs -f)
+timeout 30s grep -q 'MySQL init process done. Ready for start up.' <(docker-compose logs -f --file $COMPOSE_FILE)
+timeout 30s grep -q 'ready for connections' <(docker-compose logs -f --file $COMPOSE_FILE)
