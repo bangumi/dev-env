e70f90c63862e1af3ac6575470ad87e9a3e67470
FDKevin <fdkevinzero@gmail.com>
2022-06-06 18:45:53 +0800

    use custom file

--- wait_mysql_ready.sh
+++ wait_mysql_ready.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-cd $(dirname "$0")
-
-timeout 30s grep -q 'MySQL init process done. Ready for start up.' <(docker-compose logs -f)
-timeout 30s grep -q 'ready for connections' <(docker-compose logs -f)
+timeout 30s grep -q 'MySQL init process done. Ready for start up.' <(docker-compose -f $(dirname "$0")/docker-compose.yaml logs -f)
+timeout 30s grep -q 'ready for connections' <(docker-compose -f $(dirname "$0")/docker-compose.yaml logs -f)
