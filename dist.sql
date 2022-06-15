f284c4a66c3953d313a6cee7406e5a12afd29d51
Trim21 <Trim21@users.noreply.github.com>
2022-06-15 17:43:38 +0000

    deploy refs/heads/master 70b60062d640735d7c4f5f40a5877c19a6163ed2

--- dist.sql
+++ dist.sql
@@ -762,6 +762,36 @@ ALTER TABLE `chii_timeline` MODIFY `tml_id` INT(10) UNSIGNED NOT NULL AUTO_INCRE
 ALTER TABLE `chii_oauth_clients`
 ADD PRIMARY KEY (`app_id`),
 ADD KEY `client_id` (`client_id`);CREATE TABLE
+  IF NOT EXISTS `chii_groups` (
+    `grp_id` SMALLINT(6) UNSIGNED NOT NULL,
+    `grp_cat` SMALLINT(6) UNSIGNED NOT NULL DEFAULT '0',
+    `grp_name` CHAR(50) NOT NULL,
+    `grp_title` CHAR(50) NOT NULL,
+    `grp_icon` VARCHAR(255) NOT NULL,
+    `grp_creator` MEDIUMINT(8) UNSIGNED NOT NULL DEFAULT '0',
+    `grp_topics` MEDIUMINT(8) UNSIGNED NOT NULL DEFAULT '0',
+    `grp_posts` MEDIUMINT(8) UNSIGNED NOT NULL DEFAULT '0',
+    `grp_members` MEDIUMINT(8) UNSIGNED NOT NULL DEFAULT '1',
+    `grp_desc` TEXT NOT NULL,
+    `grp_lastpost` INT(10) UNSIGNED NOT NULL,
+    `grp_builddate` INT(10) UNSIGNED NOT NULL,
+    `grp_accessible` TINYINT(1) NOT NULL DEFAULT '1' COMMENT '可访问性',
+    `grp_nsfw` TINYINT(1) UNSIGNED NOT NULL
+  ) ENGINE = MyISAM DEFAULT CHARSET = utf8;
+
+ALTER TABLE `chii_groups`
+ADD PRIMARY KEY (`grp_id`);
+
+ALTER TABLE `chii_groups` MODIFY `grp_id` SMALLINT(6) UNSIGNED NOT NULL AUTO_INCREMENT;CREATE TABLE
+  IF NOT EXISTS `chii_group_members` (
+    `gmb_uid` MEDIUMINT(8) NOT NULL DEFAULT '0',
+    `gmb_gid` SMALLINT(6) NOT NULL DEFAULT '0',
+    `gmb_moderator` TINYINT(1) NOT NULL DEFAULT '0',
+    `gmb_dateline` INT(10) UNSIGNED NOT NULL DEFAULT '0'
+  ) ENGINE = MyISAM DEFAULT CHARSET = utf8;
+
+ALTER TABLE `chii_group_members`
+ADD PRIMARY KEY (`gmb_uid`, `gmb_gid`);CREATE TABLE
   IF NOT EXISTS `chii_os_web_sessions` (
     `key` CHAR(64) PRIMARY KEY NOT NULL COMMENT 'session key',
     #
