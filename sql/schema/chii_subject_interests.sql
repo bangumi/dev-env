6a81a3f8fee1e0df981a83d1cdddb21589559c92
Trim21 <trim21.me@gmail.com>
2022-08-06 15:50:55 +0800

    new field

--- sql/schema/chii_subject_interests.sql
+++ sql/schema/chii_subject_interests.sql
@@ -1,50 +1,70 @@
-CREATE TABLE
-  IF NOT EXISTS `chii_subject_interests` (
-    `interest_id` INT(10) UNSIGNED NOT NULL,
-    `interest_uid` MEDIUMINT(8) UNSIGNED NOT NULL,
-    `interest_subject_id` MEDIUMINT(8) UNSIGNED NOT NULL,
-    `interest_subject_type` SMALLINT(6) UNSIGNED NOT NULL DEFAULT '0',
-    `interest_rate` TINYINT(3) UNSIGNED NOT NULL DEFAULT '0',
-    `interest_type` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0',
-    `interest_has_comment` TINYINT(1) UNSIGNED NOT NULL,
-    `interest_comment` MEDIUMTEXT NOT NULL,
-    `interest_tag` MEDIUMTEXT NOT NULL,
-    `interest_ep_status` MEDIUMINT(8) UNSIGNED NOT NULL DEFAULT '0',
-    `interest_vol_status` MEDIUMINT(8) UNSIGNED NOT NULL COMMENT '卷数',
-    `interest_wish_dateline` INT(10) UNSIGNED NOT NULL,
-    `interest_doing_dateline` INT(10) UNSIGNED NOT NULL,
-    `interest_collect_dateline` INT(10) UNSIGNED NOT NULL,
-    `interest_on_hold_dateline` INT(10) UNSIGNED NOT NULL,
-    `interest_dropped_dateline` INT(10) UNSIGNED NOT NULL,
-    `interest_lasttouch` INT(10) UNSIGNED NOT NULL DEFAULT '0',
-    `interest_private` TINYINT(1) UNSIGNED NOT NULL
-  ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+create table
+  chii_subject_interests (
+    interest_id int unsigned auto_increment primary key,
+    interest_uid mediumint unsigned not null,
+    interest_subject_id mediumint unsigned not null,
+    interest_subject_type smallint(6) unsigned default 0 not null,
+    interest_rate tinyint unsigned default 0 not null,
+    interest_type tinyint(1) unsigned default 0 not null,
+    interest_has_comment tinyint(1) unsigned not null,
+    interest_comment mediumtext not null,
+    interest_tag mediumtext not null,
+    interest_ep_status mediumint unsigned default 0 not null,
+    interest_vol_status mediumint unsigned not null comment '卷数',
+    interest_wish_dateline int unsigned not null,
+    interest_doing_dateline int unsigned not null,
+    interest_collect_dateline int unsigned not null,
+    interest_on_hold_dateline int unsigned not null,
+    interest_dropped_dateline int unsigned not null,
+    interest_create_ip char(15) not null,
+    interest_lasttouch_ip char(15) not null,
+    interest_lasttouch int unsigned default 0 not null,
+    interest_private tinyint(1) unsigned not null,
+    constraint user_interest unique (interest_uid, interest_subject_id)
+  ) charset = utf8;
 
-ALTER TABLE `chii_subject_interests`
-ADD PRIMARY KEY (`interest_id`),
-ADD UNIQUE KEY `user_interest` (`interest_uid`, `interest_subject_id`),
-ADD KEY `interest_subject_id` (`interest_subject_id`, `interest_type`),
-ADD KEY `interest_uid` (`interest_uid`),
-ADD KEY `interest_collect_dateline` (`interest_collect_dateline`),
-ADD KEY `interest_private` (`interest_private`),
-ADD KEY `interest_lasttouch` (`interest_lasttouch`),
-ADD KEY `interest_subject_id_2` (`interest_subject_id`),
-ADD KEY `interest_type` (`interest_type`),
-ADD KEY `interest_subject_type` (`interest_subject_type`),
-ADD KEY `interest_rate` (`interest_rate`),
-ADD KEY `tag_subject_id` (`interest_subject_type`, `interest_type`, `interest_uid`) USING BTREE,
-ADD KEY `user_collects` (`interest_subject_type`, `interest_uid`),
-ADD KEY `subject_lasttouch` (`interest_subject_id`, `interest_private`, `interest_lasttouch`),
-ADD
-  KEY `subject_comment` (`interest_subject_id`, `interest_has_comment`, `interest_private`, `interest_lasttouch`) USING BTREE,
-ADD KEY `subject_collect` (`interest_subject_id`, `interest_type`, `interest_private`, `interest_collect_dateline`),
-ADD
-  KEY `user_collect_type` (`interest_subject_type`, `interest_type`, `interest_uid`, `interest_private`, `interest_collect_dateline`),
-ADD KEY `interest_id` (`interest_uid`, `interest_private`) USING BTREE,
-ADD KEY `subject_rate` (`interest_subject_id`, `interest_rate`, `interest_private`),
-ADD KEY `top_subject` (`interest_subject_id`, `interest_subject_type`, `interest_doing_dateline`),
-ADD KEY `user_collect_latest` (`interest_subject_type`, `interest_type`, `interest_uid`, `interest_private`),
-ADD KEY `interest_type_2` (`interest_type`, `interest_uid`),
-ADD KEY `interest_uid_2` (`interest_uid`, `interest_private`, `interest_lasttouch`);
+create index interest_collect_dateline on chii_subject_interests (interest_collect_dateline);
 
-ALTER TABLE `chii_subject_interests` MODIFY `interest_id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+create index interest_id on chii_subject_interests (interest_uid, interest_private);
+
+create index interest_lasttouch on chii_subject_interests (interest_lasttouch);
+
+create index interest_private on chii_subject_interests (interest_private);
+
+create index interest_rate on chii_subject_interests (interest_rate);
+
+create index interest_subject_id on chii_subject_interests (interest_subject_id, interest_type);
+
+create index interest_subject_id_2 on chii_subject_interests (interest_subject_id);
+
+create index interest_subject_type on chii_subject_interests (interest_subject_type);
+
+create index interest_type on chii_subject_interests (interest_type);
+
+create index interest_type_2 on chii_subject_interests (interest_type, interest_uid);
+
+create index interest_uid on chii_subject_interests (interest_uid);
+
+create index interest_uid_2 on chii_subject_interests (interest_uid, interest_private, interest_lasttouch);
+
+create index
+  subject_collect on chii_subject_interests (interest_subject_id, interest_type, interest_private, interest_collect_dateline);
+
+create index
+  subject_comment on chii_subject_interests (interest_subject_id, interest_has_comment, interest_private, interest_lasttouch);
+
+create index subject_lasttouch on chii_subject_interests (interest_subject_id, interest_private, interest_lasttouch);
+
+create index subject_rate on chii_subject_interests (interest_subject_id, interest_rate, interest_private);
+
+create index tag_subject_id on chii_subject_interests (interest_subject_type, interest_type, interest_uid);
+
+create index top_subject on chii_subject_interests (interest_subject_id, interest_subject_type, interest_doing_dateline);
+
+create index
+  user_collect_latest on chii_subject_interests (interest_subject_type, interest_type, interest_uid, interest_private);
+
+create index
+  user_collect_type on chii_subject_interests (interest_subject_type, interest_type, interest_uid, interest_private, interest_collect_dateline);
+
+create index user_collects on chii_subject_interests (interest_subject_type, interest_uid);
