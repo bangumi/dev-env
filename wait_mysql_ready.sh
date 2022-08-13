579e03ac6ed3f667a24947b690e8b1fe760cbec5
Trim21 <trim21.me@gmail.com>
2022-08-13 13:02:18 +0800

    add binlog and my

--- wait_mysql_ready.sh
+++ wait_mysql_ready.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-timeout 30s grep -q 'MySQL init process done. Ready for start up.' <(docker-compose -f $(dirname "$0")/docker-compose.yaml logs -f)
-timeout 30s grep -q 'ready for connections' <(docker-compose -f $(dirname "$0")/docker-compose.yaml logs -f)
+timeout 30s grep -q 'MySQL init process done. Ready for start up.' <(docker-compose -f "$(dirname "$0")/docker-compose.yaml" logs -f)
+timeout 30s grep -q 'ready for connections' <(docker-compose -f "$(dirname "$0")/docker-compose.yaml" logs -f)
