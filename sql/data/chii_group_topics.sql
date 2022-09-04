10a50ddf24d69e18db8f76b62553d537453f8966
Trim21 <trim21.me@gmail.com>
2022-09-04 21:28:48 +0800

    add group topics

---
+++ sql/data/chii_group_topics.sql
@@ -0,0 +1,13 @@
+INSERT INTO
+  `chii_group_topics` (
+    `grp_tpc_id`,
+    `grp_tpc_gid`,
+    `grp_tpc_uid`,
+    `grp_tpc_title`,
+    `grp_tpc_dateline`,
+    `grp_tpc_lastpost`,
+    `grp_tpc_replies`,
+    `grp_tpc_state`,
+    `grp_tpc_display`
+  )
+VALUES (371602, 4215, 287622, 'tes', 1657885648, 1662283112, 2, 2, 1);
