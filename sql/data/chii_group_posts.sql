816281f488065a9b64f7f58243495658a645a28b
Trim21 <trim21.me@gmail.com>
2022-12-19 00:41:19 +0800

    new posts

--- sql/data/chii_group_posts.sql
+++ sql/data/chii_group_posts.sql
@@ -1,11 +1,20 @@
-INSERT INTO
-  `chii_group_posts` (`grp_pst_id`, `grp_pst_mid`, `grp_pst_uid`, `grp_pst_related`, `grp_pst_content`, `grp_pst_state`, `grp_pst_dateline`)
-VALUES
-  (2055276, 371602, 287622, 0, 'tt', 0, 1657885648),
-  (2056216, 371602, 287622, 0, 't', 6, 1658030918),
-  (2092004, 371602, 287622, 0, '管理员删除', 7, 1662276469),
-  (2092065, 371602, 1, 0, '', 5, 1662282514),
-  (2092066, 371602, 1, 0, '', 1, 1662282518),
-  (2092067, 371602, 1, 0, '', 2, 1662282523),
-  (2092069, 371602, 287622, 0, 'q', 0, 1662282554),
-  (2092074, 371602, 287622, 2092069, 'sub', 0, 1662283112);
+INSERT INTO `chii_group_posts` (`grp_pst_id`, `grp_pst_mid`, `grp_pst_uid`, `grp_pst_related`,
+                                `grp_pst_content`, `grp_pst_state`, `grp_pst_dateline`)
+VALUES (2055276, 371602, 287622, 0, 'tt', 0, 1657885648),
+       (2056216, 371602, 287622, 0, 't', 6, 1658030918),
+       (2092004, 371602, 287622, 0, '管理员删除', 7, 1662276469),
+       (2092065, 371602, 1, 0, '', 5, 1662282514),
+       (2092066, 371602, 1, 0, '', 1, 1662282518),
+       (2092067, 371602, 1, 0, '', 2, 1662282523),
+       (2092069, 371602, 287622, 0, 'q', 0, 1662282554),
+       (2092074, 371602, 287622, 2092069, 'sub', 0, 1662283112);
+
+
+INSERT INTO `chii_group_posts` (`grp_pst_id`, `grp_pst_mid`, `grp_pst_uid`, `grp_pst_related`,
+                                `grp_pst_content`, `grp_pst_state`, `grp_pst_dateline`)
+VALUES (375793, 18515, 47686, 375780,
+        '[quote][b]Amarantine[/b] 说: 是啊，以及新人报道那里[/quote]\n你算计我！', 0, 1346638583);
+INSERT INTO `chii_group_topics` (`grp_tpc_id`, `grp_tpc_gid`, `grp_tpc_uid`, `grp_tpc_title`,
+                                 `grp_tpc_dateline`, `grp_tpc_lastpost`, `grp_tpc_replies`,
+                                 `grp_tpc_state`, `grp_tpc_display`)
+VALUES (375793, 4215, 287622, '测试贴 2', 1671381484, 1671381518, 2, 0, 1);
