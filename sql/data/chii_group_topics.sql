f6d4e2addbfc700a89d7926e10ad2082abd56bc3
Trim21 <trim21.me@gmail.com>
2022-12-21 02:30:54 +0800

    fix data

--- sql/data/chii_group_topics.sql
+++ sql/data/chii_group_topics.sql
@@ -1,13 +1,11 @@
-INSERT INTO
-  `chii_group_topics` (
-    `grp_tpc_id`,
-    `grp_tpc_gid`,
-    `grp_tpc_uid`,
-    `grp_tpc_title`,
-    `grp_tpc_dateline`,
-    `grp_tpc_lastpost`,
-    `grp_tpc_replies`,
-    `grp_tpc_state`,
-    `grp_tpc_display`
-  )
-VALUES (371602, 4215, 287622, 'tes', 1657885648, 1662283112, 2, 2, 1);
+INSERT INTO `chii_group_topics` (`grp_tpc_id`,
+                                 `grp_tpc_gid`,
+                                 `grp_tpc_uid`,
+                                 `grp_tpc_title`,
+                                 `grp_tpc_dateline`,
+                                 `grp_tpc_lastpost`,
+                                 `grp_tpc_replies`,
+                                 `grp_tpc_state`,
+                                 `grp_tpc_display`)
+VALUES (371602, 4215, 287622, 'tes', 1657885648, 1662283112, 2, 2, 1),
+       (375793, 4215, 287622, '测试贴 2', 1671381484, 1671381518, 2, 0, 1);
