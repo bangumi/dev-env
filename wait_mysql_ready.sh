faab70f217db503ff8b943662564188dc06eebab
Trim21 <trim21.me@gmail.com>
2023-02-15 12:52:00 +0800

    fix waiy script

--- wait_mysql_ready.sh
+++ wait_mysql_ready.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -ex
-
 timeout 30s grep -q 'MySQL init process done. Ready for start up.' <(docker-compose -f "$(dirname "$0")/docker-compose.yaml" logs -f)
 timeout 30s grep -q 'ready for connections' <(docker-compose -f "$(dirname "$0")/docker-compose.yaml" logs -f)
 
