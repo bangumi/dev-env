818cb18d69ec3b2d6156d5ee1341e3af14534a83
trim21 <trim21@users.noreply.github.com>
2023-02-15 03:53:32 +0000

    deploy refs/heads/master 945c1dcaec6b523418592836614cdb4109bf8d0c

---
+++ wait_mysql_ready.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+set -ex
+
+timeout 30s grep -q 'MySQL init process done. Ready for start up.' <(docker-compose -f "$(dirname "$0")/docker-compose.yaml" logs -f)
+timeout 30s grep -q 'ready for connections' <(docker-compose -f "$(dirname "$0")/docker-compose.yaml" logs -f)
