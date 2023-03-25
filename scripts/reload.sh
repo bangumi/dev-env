3f353805c2e26ad7f56db79500116c1cf29a5ef8
trim21 <trim21@users.noreply.github.com>
2023-03-25 05:56:06 +0000

    deploy refs/heads/master e4a7f8d3176e10383431420153a852c6bf8e0b7f

--- scripts/reload.sh
+++ scripts/reload.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-docker-compose down -v --remove-orphans
+docker-compose down -v --remove-orphans || true
+docker-compose -f mq/docker-compose.yaml down || true
+docker-compose -f micro-services/docker-compose.yaml down || true
 sudo rm -rf data
-docker-compose up --build -d
+docker-compose up -d
