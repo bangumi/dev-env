584401861eb6741dc8ec93f03405d88042d829d8
Trim21 <trim21.me@gmail.com>
2023-03-25 13:56:12 +0800

    down all service

--- dist/scripts/reload.sh
+++ dist/scripts/reload.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-docker-compose down -v --remove-orphans || true
 docker-compose -f mq/docker-compose.yaml down || true
 docker-compose -f micro-services/docker-compose.yaml down || true
+docker-compose down || true
 sudo rm -rf data
 docker-compose up -d
