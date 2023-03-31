00614cb0309a2629c3aaff31a85ffd76186f28d5
trim21 <trim21@users.noreply.github.com>
2023-03-31 12:51:53 +0000

    deploy refs/heads/master 4e9fb58e8999309db5193c528eda486283bb0304

--- dist.sql
+++ dist.sql
@@ -2768,6 +2768,7 @@ DROP TABLE IF EXISTS `chii_likes`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `chii_likes` (
   `type` mediumint(8) unsigned NOT NULL,
+  `main_id` int(10) unsigned NOT NULL,
   `related_id` int(10) unsigned NOT NULL,
   `uid` mediumint(8) unsigned NOT NULL,
   `value` mediumint(8) unsigned NOT NULL DEFAULT '0',
@@ -2775,7 +2776,8 @@ CREATE TABLE `chii_likes` (
   `created_at` int(10) unsigned NOT NULL COMMENT 'unix timestamp seconds',
   PRIMARY KEY (`type`,`related_id`,`uid`),
   KEY `idx_uid` (`uid`),
-  KEY `idx_related` (`related_id`)
+  KEY `idx_related` (`related_id`),
+  KEY `type` (`type`,`main_id`,`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
