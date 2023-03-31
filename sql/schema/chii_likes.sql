4e9fb58e8999309db5193c528eda486283bb0304
Trim21 <trim21.me@gmail.com>
2023-03-31 20:51:13 +0800

    update chii_likes

--- sql/schema/chii_likes.sql
+++ sql/schema/chii_likes.sql
@@ -1,19 +1,17 @@
-drop table if exists
-  chii_likes;
+drop table if exists chii_likes;
 
-create table IF NOT EXISTS
-  `chii_likes` (
-    `type` MEDIUMINT UNSIGNED NOT NULL,
-    `related_id` int(10) unsigned NOT NULL,
-    `uid` mediumint UNSIGNED NOT NULL,
-    `value` mediumint unsigned not null default 0,
-    `ban` tinyint(2) unsigned not null DEFAULT 0,
-    `created_at` int unsigned not null comment 'unix timestamp seconds',
-    PRIMARY KEY (type, related_id, uid)
-  ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
-
-create index
-  idx_uid on `chii_likes` (uid);
-
-create index
-  idx_related on `chii_likes` (related_id);
+CREATE TABLE IF NOT EXISTS `chii_likes`
+(
+    `type`       mediumint(8) unsigned NOT NULL,
+    `main_id`    int(10) unsigned      NOT NULL,
+    `related_id` int(10) unsigned      NOT NULL,
+    `uid`        mediumint(8) unsigned NOT NULL,
+    `value`      mediumint(8) unsigned NOT NULL DEFAULT '0',
+    `ban`        tinyint(2) unsigned   NOT NULL DEFAULT '0',
+    `created_at` int(10) unsigned      NOT NULL COMMENT 'unix timestamp seconds',
+    PRIMARY KEY (`type`, `related_id`, `uid`),
+    KEY `idx_uid` (`uid`),
+    KEY `idx_related` (`related_id`),
+    KEY `type` (`type`, `main_id`, `uid`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
