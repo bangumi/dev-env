903a4f91841675bdc532cd9a43c6c06f8f30f0e6
Trim21 <trim21.me@gmail.com>
2024-10-02 15:17:24 +0800

    update (#31)

--- sql/data/chii_apps.sql
+++ sql/data/chii_apps.sql
@@ -1,9 +1,8 @@
 LOCK TABLES
-  `chii_apps` WRITE;
+    `chii_apps` WRITE;
 
-REPLACE INTO
-  `chii_apps`
-VALUES
-  (1, 1, 2, 'Bangumi iOS', 'iOS app for Bangumi', '', 0, 0, 1715828406, 1715828406, 0);
+REPLACE INTO `chii_apps`
+VALUES (1, 1, 2, 'Bangumi iOS', 'iOS app for Bangumi', '', 0, 0, 1715828406, 1715828406,
+        0, 0);
 
 UNLOCK TABLES;
--- sql/schema/session.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS
-  `chii_os_web_sessions` (
-    `key` CHAR(64) PRIMARY KEY NOT NULL COMMENT 'session key',
-    #
-    `user_id` INT UNSIGNED NOT NULL COMMENT 'uint32 user id',
-    `value` MEDIUMBLOB NOT NULL COMMENT 'json encoded session data',
-    `created_at` BIGINT NOT NULL COMMENT 'int64 unix timestamp, when session is created',
-    `expired_at` BIGINT NOT NULL COMMENT 'int64 unix timestamp, when session is expired'
-  ) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
