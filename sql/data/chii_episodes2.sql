9575c960279344544f0673e59f2d2bb5ae623ffb
everpcpc <git@everpcpc.com>
2024-12-20 22:02:48 +0800

    chore: add more test data

---
+++ sql/data/chii_episodes2.sql
@@ -0,0 +1,4 @@
+REPLACE INTO `chii_episodes` (`ep_id`, `ep_subject_id`, `ep_sort`, `ep_type`, `ep_disc`, `ep_name`, `ep_name_cn`, `ep_rate`, `ep_duration`, `ep_airdate`, `ep_online`, `ep_comment`, `ep_resources`, `ep_desc`, `ep_dateline`, `ep_lastpost`, `ep_lock`, `ep_ban`) VALUES
+(751409, 214114, 4, 0, 0, 'Killer App', '', 0, '', '2017-10-19', '', 0, 0, '', 1506655821, 0, 0, 0),
+(751421, 214114, 16, 0, 0, 'Last Gasp', '', 0, '', '2018-03-15', '', 0, 0, '', 1506655821, 0, 0, 0),
+(1080550, 366527, 1, 0, 0, 'Pajama Day', '', 0, '22m', '2022-02-02', '', 0, 0, 'The 4th grade class is banned from wearing their pajamas to school on the most important day of the year.', 1643021844, 0, 0, 0);
