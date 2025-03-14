01d71d5f6597f8ffe9c409d902a0c3c3dcbeb0b9
trim21 <trim21@users.noreply.github.com>
2025-03-14 22:28:17 +0000

    deploy refs/heads/master 1c4e3636e93a8ff16d3117d1295dc03e22fd20e4

--- scripts/reload.sh
+++ scripts/reload.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-docker compose -f mq/docker-compose.yaml down || true
-docker compose -f micro-services/docker-compose.yaml down || true
+docker compose -f infra/docker-compose.yaml down || true
 docker compose down
+sudo rm -rf infra/data
 sudo rm -rf data
 docker compose up -d
